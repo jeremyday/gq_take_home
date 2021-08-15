edb01ff3a38c54c6ae2e6780b7837726757bca72
Jeremy Day <jeremy.day@gmail.com>
2021-08-15 10:14:09 -0500

    feat: Add a back button and improve styling a bit.

--- app/javascript/components/ResearcherApp/ResearcherApp.tsx
+++ app/javascript/components/ResearcherApp/ResearcherApp.tsx
@@ -16,13 +16,19 @@ export const ResearcherApp: React.FC = () => {
   }, []);
 
   return (
-    <div className="px-12 py-6">
-      <h1 className="text-2xl font-bold mb-6">Setup incentive</h1>
-      <p className="mb-4">Enter the coupon code for the candidate to receive:</p>
+    <>
+      <div className="px-12 py-6">
+        <a href="/" className="hover:bg-gray-100 bg-gray-200 rounded-md px-4 py-2">Back</a>
+      </div>
 
-      {loading && <span>Loading...</span>}
+      <div className="px-12 py-6">
+        <h1 className="text-2xl font-bold mb-6">Setup Incentive</h1>
+        <p className="mb-4">Enter the coupon code for the candidate to receive:</p>
 
-      {!loading && <IncentiveForm data={data} />}
-    </div>
+        {loading && <span>Loading...</span>}
+
+        {!loading && <IncentiveForm data={data} />}
+      </div>
+    </>
   );
 };
