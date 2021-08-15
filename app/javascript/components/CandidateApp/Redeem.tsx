c9be5761ea68928ec7d0d4af28d5bd94ecdb3baa
Jeremy Day <jeremy.day@gmail.com>
2021-08-15 10:47:44 -0500

    feat: Update the `Redeem` component to handle actually redeeming incentives in the database.

--- app/javascript/components/CandidateApp/Redeem.tsx
+++ app/javascript/components/CandidateApp/Redeem.tsx
@@ -1,21 +1,35 @@
 import * as React from 'react';
 import { useState } from 'react';
+import { redeemIncentive } from '@api/endpoints';
 
 interface Props {
   data: Incentive[];
 }
 export const Redeem: React.FC<Props> = ({ data }) => {
-  const [redeemed, setRedeemed] = useState(false);
+  const [message, setMessage] = useState('');
+  const [saving, setSaving] = useState(false);
 
   async function handleClickRedeem() {
-    setRedeemed(true);
+    setSaving(true);
+
+    if(data.length == 0) {
+      setMessage(`There are no more codes to redeem! Try back again later. Sorry for the inconvenience.`);
+
+      return;
+    }
+
+    const incentive = data.pop();
+
+    redeemIncentive(incentive.id).then(() => setMessage(`Your code is: ${incentive.code}. Thanks for participating in our research!`));
+
+    setSaving(false);
   }
 
   return (
     <div>
       <div className="pb-4">
         <button
-          disabled={redeemed}
+          disabled={saving}
           className="hover:bg-gray-100 bg-gray-200 rounded-md px-4 py-2"
           onClick={handleClickRedeem}
         >
@@ -23,9 +37,9 @@ export const Redeem: React.FC<Props> = ({ data }) => {
         </button>
       </div>
 
-      {redeemed && (
+      {message && (
         <div className="py-4 text-green-600 italic">
-          Your code is: {data[0].code}. Thanks for participating in our research!
+          {message}
         </div>
       )}
     </div>
