d03fcbb5b8b6860b4616bdf32893950ae66561bd
Jeremy Day <jeremy.day@gmail.com>
2021-08-15 10:13:01 -0500

    feat: Add functionality to create `Incentive` models and create new incentive codes.

--- app/controllers/api/incentives_controller.rb
+++ app/controllers/api/incentives_controller.rb
@@ -1,4 +1,10 @@
 class Api::IncentivesController < ApplicationController
+  def create
+    @incentive  = Incentive.create! code: params[:code]
+    @incentives = Incentive.all
+
+    render json: @incentives.to_json
+  end
 
   def index
     @incentives = Incentive.all
@@ -6,10 +12,17 @@ class Api::IncentivesController < ApplicationController
     render json: @incentives.to_json
   end
 
+  def new
+    @incentive = Incentive.new code: Nanoid.generate(size: 8)
+
+    render json: @incentive.to_json
+  end
+
   def update
     @incentive = Incentive.find(params[:id])
 
     @incentive.update!(update_params)
+
     render json: @incentive.to_json
   end
 
