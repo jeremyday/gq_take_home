681e80ac0f438f13809db1554dd04f6472411913
Jeremy Day <jeremy.day@gmail.com>
2021-08-15 10:14:24 -0500

    fix: Update some code style.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: {format: :json} do
+  namespace :api, defaults: { format: :json } do
     resources :incentives
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
